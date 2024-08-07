ca037bbd50d777f954e306a67ca6a23e47bf9932
Ammar Hassan <homesuper7@gmail.com>
2024-08-07 21:55:27 +0500

    Add changes to root layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,11 +35,6 @@ export default function RootLayout({
     }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-          <header>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-          </header>
           {children}
         </body>
       </html>
