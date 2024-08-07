35d01cc0fe99d988c07f93779c8fda6efdfe2a27
Ammar Hassan <homesuper7@gmail.com>
2024-08-07 16:36:33 +0500

    Add styling to sidebar nav and shadcn button

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -5,6 +5,7 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -65,12 +66,15 @@ const Sidebar = () => {
               })}
 
               <li className="flex-center cursor-pointer gap-2 p-4">
-                <UserButton afterSignOutUrl='/' showName />
+                <UserButton showName />
               </li>
             </ul>
           </SignedIn>
 
           <SignedOut>
+            <Button asChild className='button bg-purple-gradient bg-cover'>
+              <Link href="/sign-in">Login</Link>
+            </Button>
           </SignedOut>
         </nav>
       </div>
