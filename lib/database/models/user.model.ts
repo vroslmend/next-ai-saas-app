1c7f8125d1e0c44484fea8ea79d025c4c29ce1c5
Ammar Hassan <homesuper7@gmail.com>
2024-08-09 16:33:42 +0500

    Fixed naming error for clerkID => clerkId

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,7 +1,7 @@
 import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema({
-    clerkID: { type: String, required: true, unique: true },
+    clerkId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     photo: { type: String, required: true  },
