e40fb7138470047848f90de6fd234d763b85c052
Ammar Hassan <homesuper7@gmail.com>
2024-08-12 22:51:45 +0500

    Add Header component and fixed routing folder structure

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,18 @@
+import Header from '@/components/shared/Header'
+import React from 'react'
+import { transformationTypes } from '@/constants'
+
+const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+
+  return (
+    <>
+      <Header 
+        title={transformation.title}
+        subtitle={transformation.subTitle} 
+      />
+    </>
+  )
+}
+
+export default AddTransformationTypePage
