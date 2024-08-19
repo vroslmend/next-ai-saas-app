e73b160990cb8b226a0d558491f81a8b29b717e0
Ammar Hassan <homesuper7@gmail.com>
2024-08-19 10:27:47 +0500

    Add shadcn Form Component

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,18 +1,18 @@
-import Header from '@/components/shared/Header'
-import React from 'react'
-import { transformationTypes } from '@/constants'
+import Header from "@/components/shared/Header";
+import React from "react";
+import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
 
 const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
   const transformation = transformationTypes[type];
 
   return (
     <>
-      <Header 
-        title={transformation.title}
-        subtitle={transformation.subTitle} 
-      />
-    </>
-  )
-}
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default AddTransformationTypePage
+      <TransformationForm />
+    </>
+  );
+};
+
+export default AddTransformationTypePage;
